de48e9d4ff362f4d04c84b1e2f46f511c40988b3
Rahul M V <rahulmudambi@gmail.com>
2016-11-02 13:30:26 +0530

    more POP progress

--- POP_Project/TodoApp/src/todoApp/TodoModel.java
+++ POP_Project/TodoApp/src/todoApp/TodoModel.java
@@ -1,19 +1,20 @@
 package todoApp;
 
-import java.io.File;
-import java.io.IOException;
+import java.io.*;
+//import java.io.IOException;
 
 public class TodoModel {
 	
-	public boolean checkIfCategoryExists(String s)
+	public boolean checkIfCategoryExists(String s,int n)
 	{
 	try
 	{
-		String path = "/home/student/229/College_Project/POP_Project/TodoApp/src/todoApp/" + s;
+		String path = "C:/Users/Rahul/Desktop/College_project/POP_Project/TodoApp/src/todoApp/" + s;
 		File f = new File(path);
 		if(f.exists())
 			return false;
-		else {f.createNewFile();return true;}
+		else if(n==1){f.createNewFile();return true;}
+		else return true;
 	}
 	
 	catch(IOException ioe)
@@ -22,6 +23,38 @@ public class TodoModel {
 		return false;
 	}
 	
+	}
+	
+	public  String addTask(TaskBean name,String catName)
+	{
+		try
+		{
+		String path = "C:/Users/Rahul/Desktop/College_project/POP_Project/TodoApp/src/todoApp/" + catName;
+		FileOutputStream fileOut = new FileOutputStream(path);
+		ObjectOutputStream out = new ObjectOutputStream(fileOut);
+		out.writeObject(name);
+		out.close();
+		fileOut.close();
+		FileInputStream fileIn = new FileInputStream(path);
+		ObjectInputStream in = new ObjectInputStream(fileIn);
+		TaskBean tb = (TaskBean)(in.readObject());
+		System.out.println("Name = "+tb.getName());
+		System.out.println("desc: "+tb.getDesc());
+		in.close();
+		return null;
+		}
+		catch(IOException ioe)
+		{
+			ioe.printStackTrace();
+		}
+		catch(ClassNotFoundException c)
+		{
+			System.out.println("NO class found...");
+			
+		}
+		
+		return null;
+		
 	}
 
 }
